45db9d3eaec8c6b7994eb40853399e7a74701dca
Beet-le <1693332907@qq.com>
2021-10-22 19:48:40 +0800

    1021

--- src/test/java/com/bt/dao/UserDaoTest.java
+++ src/test/java/com/bt/dao/UserDaoTest.java
@@ -1,4 +1,29 @@
 package com.bt.dao;
 
+import com.bt.pojo.User;
+import com.bt.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
 public class UserDaoTest {
+    @Test
+    public void test() {
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭SqlSession
+        sqlSession.close();
+
+    }
+
 }
